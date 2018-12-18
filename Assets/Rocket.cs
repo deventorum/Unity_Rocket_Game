9c63a83e7fb4812383c5521d3e68b92240908e52
deventorum <deventorum@gmail.com>
2018-12-18 16:00:11 -0500

    Audio added

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -3,12 +3,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(AudioSource))]
 public class Rocket : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+    Rigidbody rigidBody;
+    AudioSource audioData;
+
+    // Use this for initialization
+    void Start () {
+        rigidBody = GetComponent<Rigidbody>();
+        audioData = GetComponent<AudioSource>();
+    }
 	
 	// Update is called once per frame
 	void Update () {
@@ -17,16 +22,27 @@ public class Rocket : MonoBehaviour {
 
     private void ProcessInput()
     {
-        if (Input.GetKey(KeyCode.Space)) {
-            print("thrusting");
-        }
+        ThrustersEngaged();
         if (Input.GetKey(KeyCode.A))
         {
-            print("rotating left");
+            transform.Rotate(Vector3.forward);
         }
         if (Input.GetKey(KeyCode.D))
         {
-            print("rotating right");
+            transform.Rotate(-Vector3.forward);
         }
+    }
+
+    private void ThrustersEngaged()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            rigidBody.AddRelativeForce(Vector3.up);
+            if (!audioData.isPlaying)
+            {
+                audioData.Play(0);
+            }
+        }
+        else audioData.Stop();
     }
 }
