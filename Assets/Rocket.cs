2563111d1d2d15160f7d7c261fb4900aace50a5f
deventorum <deventorum@gmail.com>
2018-12-18 18:27:01 -0500

    Collide interaction is added

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -8,11 +8,17 @@ public class Rocket : MonoBehaviour {
 
     Rigidbody rigidBody;
     AudioSource audioData;
+    AudioSource collisionAudio;
+    [SerializeField] float MainThrust = 100f; 
+
+    [SerializeField] float RcsThrust = 100f;
+
 
     // Use this for initialization
     void Start () {
         rigidBody = GetComponent<Rigidbody>();
         audioData = GetComponent<AudioSource>();
+        collisionAudio = GetComponent<AudioSource>();
     }
 	
 	// Update is called once per frame
@@ -23,26 +29,55 @@ public class Rocket : MonoBehaviour {
     private void ProcessInput()
     {
         ThrustersEngaged();
-        if (Input.GetKey(KeyCode.A))
-        {
-            transform.Rotate(Vector3.forward);
-        }
-        if (Input.GetKey(KeyCode.D))
-        {
-            transform.Rotate(-Vector3.forward);
-        }
+        RotateRocket();
     }
-
     private void ThrustersEngaged()
     {
         if (Input.GetKey(KeyCode.Space))
         {
-            rigidBody.AddRelativeForce(Vector3.up);
+            rigidBody.AddRelativeForce(Vector3.up * MainThrust);
             if (!audioData.isPlaying)
             {
                 audioData.Play(0);
             }
         }
-        else audioData.Stop();
+        else audioData.Pause();
     }
+
+    private void RotateRocket()
+    {
+        rigidBody.freezeRotation = true; // take manual control of rotation
+        float rotationThisFrame = RcsThrust * Time.deltaTime;
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(Vector3.forward * rotationThisFrame);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(-Vector3.forward * rotationThisFrame);
+        }
+        rigidBody.freezeRotation = false;
+    }
+    // Blocks x and y rotation
+    protected void LateUpdate()
+    {
+        transform.localEulerAngles = new Vector3(0, 0, transform.localEulerAngles.z);
+    }
+    void OnCollisionEnter(Collision collision)
+    {
+        switch (collision.gameObject.tag)
+        {
+            case "Friendly":
+                print("OK"); //todo remove this line
+                break;
+            case "Fuel":
+                print("Fuel");
+                break;
+            default:
+                print("Dead");
+                break;
+        }
+    }
+
 }
